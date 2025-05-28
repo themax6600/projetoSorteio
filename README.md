3b2b47872c6e57233940e7f3011b5c66de18e2b6
themax6600 <max.2935@aluno.pr.senac.br>
2025-05-28 12:12:58 -0300

    omg

--- README.md
+++ README.md
@@ -3,6 +3,4 @@
 Aqui é o grupo q vai fazer o melhor✌️ sorteio pro sesquiiiiiiiiiiiiiiiii
 vou ganhar
 
-e o henrique vai ficar em ultimo como de cost
-
 Renato nao vai ganhar pq eu posso apagar todas as mensagens dele e contar ponto negativo
--- assets/pages/listaAdm.php
+++ assets/pages/listaAdm.php
@@ -6,31 +6,31 @@ include_once('../templates/header.php');
 <main>
     <div class="container">
         <div class="col d-flex flex-row text-center mt-4">
-            <a href="<?= BASE_URL?>assets/pages/main.php"><img src="../img/seta.png" alt=""></a>
+            <a href="<?= BASE_URL ?>assets/pages/main.php"><img src="../img/seta.png" alt=""></a>
             <h4 class="w-100 d-flex justify-content-center align-items-center">Lista de Alunos</h4>
         </div>
         <div class="col">
             <div class="card-group mt-4 ">
-                <?php foreach($userinfos as $userinfo) {?>
-                    <?php if($userinfo['adm'] == 0) {?>
-                <div class="container ps-2 mb-3">
-                    <div class="row">
-                        <div class="col border border-dark border-2">
-                            <?= $userinfo['userName']; ?>
+                <?php foreach ($userinfos as $userinfo) { ?>
+                    <?php if ($userinfo['adm'] == 0) { ?>
+                        <div class="container ps-2 mb-3">
+                            <div class="row">
+                                <div class="col border border-dark border-2">
+                                    <?= $userinfo['userName']; ?>
+                                </div>
+                                <div class="col border border-dark border-2">
+                                    Email: <?= $userinfo['userEmail']; ?>
+                                </div>
+                                <div class="col border border-dark border-2">
+                                    CPF: <?= $userinfo['userCpf']; ?>
+                                </div>
+                                <div class="col border border-dark border-2">
+                                    Idade: <?= $userinfo['userName']; ?>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col border border-dark border-2">
-                            Email: <?= $userinfo['userEmail']; ?>
-                        </div>
-                        <div class="col border border-dark border-2">
-                            CPF: <?= $userinfo['userCpf']; ?>
-                        </div>
-                        <div class="col border border-dark border-2">
-                            Idade: <?= $userinfo['userName']; ?>
-                        </div>
-                    </div>
-                </div>
-                    <?php };?>
-                <?php };?>
+                    <?php }; ?>
+                <?php }; ?>
             </div>
         </div>
     </div>
