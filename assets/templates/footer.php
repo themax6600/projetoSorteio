3bddcc7fdfdf2394beb651f79f985559caaf731e
henrique030308 <henrique.2910@aluno.>
2025-05-28 12:13:21 -0300

    çlç

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    
+    <img src="../img/ondinha.png" alt="">
 </footer>
 <script src="<?= BASE_URL ?>assets/js/script.js"></script>
 <script src="<?= BASE_URL ?>assets/bootstrap/js/bootstrap.bundle.min.js"></script>
