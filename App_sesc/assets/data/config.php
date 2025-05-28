6c87fd068bee26f370fb23f152c86b3c45d874a7
themax6600 <max.2935@aluno.pr.senac.br>
2025-05-28 11:46:34 -0300

    28/05

--- App_sesc/assets/data/config.php
+++ App_sesc/assets/data/config.php
@@ -10,6 +10,6 @@ $conexao = new PDO("mysql:host=$dbHost; dbname=$dbNome; charset=utf8", $dbUser, 
     echo 'Erro ao conectar com o banco de dados: ' . $erro->getMessage();
 }
 
-define('BASE_URL', 'http://172.17.34.253:1200/projetos/202300005/alunos/max/3%c2%ba%20ano/App_sesc/');
+define('BASE_URL', 'http://172.17.34.253:1200/projetos/202300005/alunos/max/3%c2%ba%20ano/projetoSorteio/App_sesc/');
 
 ?>
